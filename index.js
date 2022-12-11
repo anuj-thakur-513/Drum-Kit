82b8706fecd40b9089b50a68d9753bc538b6d986
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-12-12 02:04:08 +0530

    playing the sounds on button clicks using DOM

--- index.js
+++ index.js
@@ -4,8 +4,42 @@ function handleClick() {
 }
 
 // setting onClick event listener for every element of button attribute
-let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (let i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function playSound() {
+
+        // getting the inner HTML of the button pressed 
+        var buttonInnerHTML = this.innerHTML;
+        // variable to store the audio to be played
+        var audio;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                audio = new Audio("sounds/tom-1.mp3");
+                break;
+            case "a":
+                audio = new Audio("sounds/tom-2.mp3");
+                break;
+            case "s":
+                audio = new Audio("sounds/tom-3.mp3");
+                break;
+            case "d":
+                audio = new Audio("sounds/tom-4.mp3");
+                break;
+            case "j":
+                audio = new Audio("sounds/snare.mp3");
+                break;
+            case "k":
+                audio = new Audio("sounds/crash.mp3");
+                break;
+            case "l":
+                audio = new Audio("sounds/kick-bass.mp3");
+                break;
+            default:
+                break;
+        }
+        // playing the audio after storing the audio
+        audio.play();
+    });
 }
